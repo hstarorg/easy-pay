fc51bc419a478312ecfaa555a770036250e140ba
Hstarorg <hm910705@163.com>
2018-12-02 01:45:57 +0800

    web project init with cra2.x

---
+++ webfront/config-overrides.js
@@ -0,0 +1,12 @@
+const ccra = require('customize-cra');
+module.exports = ccra.override(
+  ccra.useEslintRc(),
+  ccra.useBabelRc(),
+  ccra.addDecoratorsLegacy(),
+  // ccra.fixBabelImports('antd', { libraryDirectory: 'es', style: 'css' }),
+  ccra.fixBabelImports('antd', { libraryDirectory: 'es', style: true }), // If need customize style
+  ccra.addLessLoader({
+    modifyVars: {},
+    javascriptEnabled: true
+  })
+);
